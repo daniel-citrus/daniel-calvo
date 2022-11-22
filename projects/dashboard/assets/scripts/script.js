2dfd475d9944b1cd9aef78ffad0152d4d305e05e
Daniel <dandcalvo@gmail.com>
2022-11-22 14:12:20 -0800

    Days until rent due

--- projects/dashboard/assets/scripts/script.js
+++ projects/dashboard/assets/scripts/script.js
@@ -3,3 +3,15 @@ let year = document.querySelectorAll('.current-year');
 for (let y of year) {
     y.textContent = new Date().getUTCFullYear();
 }
+
+function getDaysUntilNextMonth() {
+    const date = new Date();
+
+    let timeDiff = new Date(date.getFullYear(), date.getMonth() + 1, 1) - date.getTime();
+
+    return Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+}
+
+let rentDue = document.getElementById('rent-due');
+rentDue.textContent = getDaysUntilNextMonth();
